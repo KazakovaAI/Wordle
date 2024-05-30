02474aab1adbfeac9385abae1db8c2e08ae9aae9
KazakovaAI <a.kazakova2@stud.uniyar.ac.ru>
2024-05-30 05:36:58 +0300

    Add keyboard buttons

--- Wordle/Source.cpp
+++ Wordle/Source.cpp
@@ -93,17 +93,21 @@ private:
 	double y = 0;
 	double width = 0;
 	double height = 0;
-	const char* text = nullptr;
+	string text = " ";
 public:
 	vector <double> color = { 0.2, 0.2, 0.7 };
 	Button() = default;
-	Button(double x, double y, double width, double height, const char* text) : x(x), y(y), width(width), height(height), text(text) {}
-	void change(const double& xi, const double& yi, const double& h, const double& w, const char* s)
+	Button(double x, double y, double width, double height, string text) : x(x), y(y), width(width), height(height), text(text) {}
+	void change(const double& xi, const double& yi, const double& h, const double& w, string s)
 	{
 		x = xi;
 		y = yi;
 		height = h;
 		width = w;
+		text = s;
+	}
+	void change_text(string s)
+	{
 		text = s;
 	}
 	void setColor(double first, double second, double third) {
@@ -121,11 +125,15 @@ public:
 		glVertex2f(x, y + height);
 		glEnd();
 		glColor3f(text_color[0], text_color[1], text_color[2]);
-		int size = strlen(text);
+		int size = text.size();
 		float textX = x;
 		if (text == "QUIT")
 		{
 			textX += 45;
+		}
+		else if (size == 1)
+		{
+			textX += 19;
 		}
 		float textY = (2 * y + height) / 2 - 9;
 		glRasterPos2f(textX, textY);
@@ -179,6 +187,121 @@ Button Buttons::let6;
 vector<Button> Buttons::keyboard(26);
 vector<Button> Buttons::fields(36);
 
+class Input {
+public:
+	static vector<vector<char>> Current_letters;
+	static int words_inputed;
+	static vector<vector<int>> Right;
+	static vector<char> keyboard_place;
+	static vector<int> Mark;
+};
+vector<vector<char>> Input::Current_letters(6);
+vector<vector<int>> Input::Right(6);
+int Input::words_inputed = 0;
+vector<char> Input::keyboard_place = { 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M' };
+vector<int> Input::Mark(26);
+
+void DrawKeyboard(int num)
+{
+	if (Input::Mark[num] == 0)
+	{
+		Buttons::keyboard[num].setColor(1.0, 1.0, 0.95);
+	}
+	else if (Input::Mark[num] == 1)
+	{
+		Buttons::keyboard[num].setColor(0.6, 0.6, 0.6);
+	}
+	else if (Input::Mark[num] == 2)
+	{
+		Buttons::keyboard[num].setColor(1.0, 1.0, 0.0);
+	}
+	else
+	{
+		Buttons::keyboard[num].setColor(0.0, 1.0, 0.0);
+	}
+	Buttons::keyboard[num].drawButton(Buttons::keyboard[num].color, {0.0, 0.0, 0.0});
+	glLineWidth(1);
+	glBegin(GL_LINES);
+	glColor3f(0.0, 0.0, 0.0);
+	int x = -378;
+	int y = -120;
+	if (num >= 10 && num < 19)
+	{
+		x = -378 + 34.5;
+		y = -120 - 67;
+		num %= 10;
+	}
+	else if (num >= 19)
+	{
+		x = -378 + 34.5 + 67;
+		y = -120 - 67 - 67;
+		num %= 19;
+	}
+	glVertex2f(x + num * 50 + num * 17, y);
+	glVertex2f(x + num * 50 + num * 17, y + 50);
+	glVertex2f(x + num * 50 + num * 17, y + 50);
+	glVertex2f(x + num * 50 + num * 17 + 50, y + 50);
+	glVertex2f(x + num * 50 + num * 17 + 50, y + 50);
+	glVertex2f(x + num * 50 + num * 17 + 50, y);
+	glVertex2f(x + num * 50 + num * 17 + 50, y);
+	glVertex2f(x + num * 50 + num * 17, y);
+	glEnd();
+	glFlush();
+}
+
+void DrawFields(int num)
+{
+	if (Input::Current_letters[num / Window::size].size() > num % Window::size)
+	{
+		char ch = Input::Current_letters[num / Window::size][num % Window::size];
+		string s = "";
+		s += ch;
+		Buttons::fields[num].change_text(s);
+		if (Input::Right[num / Window::size][num % Window::size] == 0)
+		{
+			Buttons::fields[num].setColor(1.0, 1.0, 0.95);
+		}
+		else if (Input::Right[num / Window::size][num % Window::size] == 1)
+		{
+			Buttons::fields[num].setColor(1.0, 1.0, 0);
+		}
+		else if (Input::Right[num / Window::size][num % Window::size] == 2)
+		{
+			Buttons::fields[num].setColor(0.0, 1.0, 0);
+		}
+	}
+	else
+	{
+		Buttons::fields[num].setColor(1.0, 1.0, 0.95);
+	}
+	Buttons::fields[num].drawButton(Buttons::fields[num].color, { 0.0, 0.0, 0.0 });
+	glLineWidth(1);
+	glBegin(GL_LINES);
+	glColor3f(0.0, 0.0, 0.0);
+	int x = -150;
+	int y = 250;
+	if (Window::size == 4)
+	{
+		x = -100;
+	}
+	else if (Window::size == 5)
+	{
+		x = -125;
+	}
+	int i = num / Window::size;
+	int j = num % Window::size;
+	glVertex2f(x + j * 50, y - i * 50);
+	glVertex2f(x + j * 50, y - i * 50 + 50);
+	glVertex2f(x + j * 50, y - i * 50 + 50);
+	glVertex2f(x + j * 50 + 50, y - i * 50 + 50);
+	glVertex2f(x + j * 50 + 50, y - i * 50 + 50);
+	glVertex2f(x + j * 50 + 50, y - i * 50);
+	glVertex2f(x + j * 50 + 50, y - i * 50);
+	glVertex2f(x + j * 50, y - i * 50);
+	glEnd();
+	glFlush();
+}
+
 void Button_set()
 {
 	Buttons::quit.change(-70, -250, 50, 140, "QUIT");
@@ -199,8 +322,29 @@ void Button_set()
 	{
 		for (int j = 0; j < Window::size; ++j)
 		{
-			Buttons::fields[i * Window::size + j].change(x + j * 50, y - i * 50, 50, 50, "G");
+			Buttons::fields[i * Window::size + j].change(x + j * 50, y - i * 50, 50, 50, " ");
 		}
+	}
+	x = -378;
+	y = -120;
+	for (int i = 0; i < 26; ++i)
+	{
+		int d = i;
+		if (i >= 10 && i < 19)
+		{
+			x = -378 + 34.5;
+			y = -120 - 67;
+			d %= 10;
+		}
+		else if (i >= 19)
+		{
+			x = -378 + 34.5 + 67;
+			y = -120 - 67 - 67;
+			d %= 19;
+		}
+		string s = "";
+		s += Input::keyboard_place[i];
+		Buttons::keyboard[i].change(x + d * 50 + d * 17, y, 50, 50, s);
 	}
 }
 
@@ -247,7 +391,7 @@ void display()
 	glEnd();
 	if (Window::current == "Main menu")
 	{
-		Buttons::quit.drawButton({1.0, 0.0, 0.0});
+		Buttons::quit.drawButton({ 1.0, 0.0, 0.0 });
 		Buttons::let4.drawButton({ 0.0, 1.0, 0.0 });
 		Buttons::let5.drawButton({ 0.0, 1.0, 0.0 });
 		Buttons::let6.drawButton({ 0.0, 1.0, 0.0 });
@@ -256,7 +400,11 @@ void display()
 	{
 		for (int i = 0; i < Window::size * 6; ++i)
 		{
-			Buttons::fields[i].drawButton({ 0.0, 0.0, 0.05 });
+			DrawFields(i);
+		}
+		for (int i = 0; i < 26; ++i)
+		{
+			DrawKeyboard(i);
 		}
 	}
 	glutSwapBuffers();
